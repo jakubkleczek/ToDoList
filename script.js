c5619a8b65984c2df614d04292b820a807be0760
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-02-17 20:23:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const input = document.getElementById('input');
+const toDoList = [];
+
+function addItem() {
+    toDoList.push(input.value);
+    console.log(toDoList);
+    // wyczysicc input po dodaniu
+}
+
+// removeItem function
+
+// edit function 
+
+// aray
