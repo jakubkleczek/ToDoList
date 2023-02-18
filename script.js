5427b9dc5630e609be4cf28688cb220a5b4cba71
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-02-18 15:30:50 +0100

    change script.js

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const toDoList = [];
 function addItem() {
     toDoList.push(input.value);
     console.log(toDoList);
+    document.getElementById('input').value='';
     // wyczysicc input po dodaniu
 }
+
+
 
 // removeItem function
 
 // edit function 
 
-// aray
+// array
